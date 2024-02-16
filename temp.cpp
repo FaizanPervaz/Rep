09051f8c299b6edf8da1489f2682fb5d91bf4607
FaizanPervaz <faizan.pervaz@gmail.com>
2024-02-16 17:20:40 +0500

    temp file 2

--- temp.cpp
+++ temp.cpp
@@ -1,51 +1,7 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int a[3][3], b[3][3], c[3][3];
-    cout << "Enter the first matrix: ";
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            cin >> a[i][j];
-        }
-    }
-    cout << "Enter the second matrix: ";
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            cin >> b[i][j];
-        }
-    }
-    cout << "The first matrix is: " << endl;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            cout << a[i][j] << " ";
-        }
-        cout << endl;
-    }
-    cout << "The second matrix is: " << endl;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            cout << b[i][j] << " ";
-        }
-        cout << endl;
-    }
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            c[i][j] = 0;
-            for (int k = 0; k < 3; k++) {
-                c[i][j] += a[i][k] * b[k][j];
-            }
-        }
-    }
-    cout << "The resultant matrix is: " << endl;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            cout << c[i][j] << " ";
-        }
-        cout << endl;
-    }
-    return 0;
-}
-int main() {
+    //hehe
     int a[3][3], b[3][3], c[3][3];
     cout << "Enter the first matrix: ";
     for (int i = 0; i < 3; i++) {
