21ae73f42c52335630e5fafadddf3379bcb2146a
Faizan Pervaz <121532370+FaizanPervaz@users.noreply.github.com>
2024-05-05 17:36:59 +0500

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,19 @@
+#include<gtest/gtest.h>
+
+TEST(Task1, TestCase1) {
+    int a = 5;
+    int b = 3;
+    EXPECT_EQ(add(a, b), 8);
+}
+
+TEST(Task1, TestCase2) {
+    int a = -5;
+    int b = 3;
+    EXPECT_EQ(add(a, b), -2);
+}
+
+TEST(Task1, TestCase3) {
+    int a = 0;
+    int b = 4;
+    EXPECT_EQ(add(a, b), 4);
+}
