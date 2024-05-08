da07640bbd3c21ff59f2ac412c47be9dda6a6d04
HaiderYar <128994427+HaiderYar@users.noreply.github.com>
2024-05-08 22:33:45 +0500

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -7,18 +7,6 @@ TEST(Task1, TestCase1) {
     EXPECT_EQ(add(5, 3), 8);
 }
 
-TEST(Task1, TestCase2) {
-    int a = -5;
-    int b = 3;
-    EXPECT_EQ(add(3, -5), -2);
-}
-
-TEST(Task1, TestCase3) {
-    int a = 0;
-    int b = 4;
-    EXPECT_EQ(add(0, 4), 4);
-}
-
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
