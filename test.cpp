b124511b25698959332d30c5f228d28662677a68
HaiderYar <128994427+HaiderYar@users.noreply.github.com>
2024-05-08 22:32:33 +0500

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -4,19 +4,19 @@
 TEST(Task1, TestCase1) {
     int a = 5;
     int b = 3;
-    EXPECT_EQ(add(a, b), 8);
+    EXPECT_EQ(add(5, 3), 8);
 }
 
 TEST(Task1, TestCase2) {
     int a = -5;
     int b = 3;
-    EXPECT_EQ(add(a, b), -2);
+    EXPECT_EQ(add(3, -5), -2);
 }
 
 TEST(Task1, TestCase3) {
     int a = 0;
     int b = 4;
-    EXPECT_EQ(add(a, b), 4);
+    EXPECT_EQ(add(0, 4), 4);
 }
 
 int main(int argc, char **argv) {
